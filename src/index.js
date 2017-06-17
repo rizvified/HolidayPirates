b44b5634130230fa625493cce995fc46294fc6a0
rizvified <rizvvified@gmail.com>
2017-06-17 06:27:32 +0500

    hotel box template

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import axios from 'axios';
 import { hotelBox, errorBox } from './template';
 
-
 $('.load_hotels').on('click', function(e) {
   e.preventDefault();
   _getHotels();
 });
 
 const _getHotels = () => {
+  if($('.api_error') !== null) $('.api_error').remove();
   axios.get('http://fake-hotel-api.herokuapp.com/api/hotels?count=5')
   .then(function (response) {
     const hotels = response.data.map((hotel, index) =>{
@@ -29,7 +29,18 @@ const _getHotels = () => {
     });
   })
   .catch(function (error) {
-    const alert = errorBox(error);
+    const alert = errorBox();
     $( ".hotel_list" ).append(alert);
- });
-}
+ })
+};
+
+const _getReviews = (id) => {
+  axios.get(`http://fake-hotel-api.herokuapp.com/api/reviews?hotel_id=${id}`)
+  .then(function (response) {
+      console.log(response);
+      $(`.${id} .reviews`).append(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+ })
+};
--- src/template.js
+++ src/template.js
@@ -1,19 +1,67 @@
-const hotelBox = (...details) => {
-  return (
-    `<div class="row">
-      <h2 class="col-md-offset4 col-md-4">
-        ${details[0]}
-      </h2>
-    </div>`
-  )
+const hotelBox = (id, name, description, date_start, date_end, price, rating, stars, city, country, images) => {
+  return `
+    <div class="row">
+      <div class="hotel ${id}">
+        <div class="col-md-4 hotel_image">
+          <img src="${images[0]}" class="img-responsive" alt="hotel"  />
+        </div>
+        <div1 class="col-md-8 hotel_details">
+          <div class="row">
+            <div class="col-md-7">
+              <div class="row">
+                <div class="col-md-12 hotel_name">
+                  <h2>
+                    ${name}
+                  </h2>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-md-12 hotel_location">
+                  ${city} - ${country}
+                </div>
+              </div>
+            </div>
+            <div class="col-md-5 hotel_rating">
+              ${stars}
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-12 hotel_description">
+              <p>
+                ${description}
+              </p>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-5">
+              <button type="button" class="btn btn-primary show_reviews">Load Reviews</button>
+            </div>
+            <div class="col-md-7">
+              <div class="row">
+                <div class="col-md-12 hotel_price">
+                  ${price}
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-md-12 hotel_dates">
+                  ${date_start} - ${date_end}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    `
 };
 
-const errorBox = (error) => {
+const errorBox = () => {
   return `
-    <div class="alert alert-danger" role="alert">
-      ${error}
+    <div class="alert alert-warning alert-dismissible api_error" role="alert">
+      <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+      An error occured
     </div>
-  `
+    `
 }
 
 export {
